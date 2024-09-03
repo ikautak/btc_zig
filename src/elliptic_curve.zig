ed55fbfc852452c2c7d9a2ba58283e557f1e962e
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2024-09-03 23:55:25 +0900

    add verify test

--- src/elliptic_curve.zig
+++ src/elliptic_curve.zig
@@ -3,14 +3,28 @@ const testing = std.testing;
 const FieldElement = @import("field_element.zig");
 const Point = @import("point.zig");
 
+pub fn modmul(comptime T: type, a: T, b: T, m: T) T {
+    comptime if (@bitSizeOf(T) > 256) {
+        @compileError("The type size cannot be larger than u256.");
+    };
+
+    const a_: u512 = @intCast(a);
+    const b_: u512 = @intCast(b);
+    const n: u512 = (a_ * b_) % m;
+    const result: T = @truncate(n);
+    return result;
+}
+
 pub fn modpow(comptime T: type, x: T, exp: T, m: T) T {
     var result: T = 1;
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * x) % m;
+    var x_ = x;
+    var exp_ = exp;
+    while (exp_ > 0) {
+        if (exp_ % 2 == 1) {
+            result = modmul(T, result, x_, m);
         }
-        x = (x * x) % m;
-        exp /= 2;
+        x_ = modmul(T, x_, x_, m);
+        exp_ /= 2;
     }
 
     return result;
@@ -22,16 +36,17 @@ const P: u256 = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc
 const N: u256 = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141;
 
 const S256Field = FieldElement.FieldElement(u256);
-//pub fn S256Field() type {
-//    return struct {
-//        field: FieldElement.FieldElement(u256),
-//
-//        pub fn sqrt(self: @This()) @This() {
-//            const p = @divTrunc(P + 1, 4);
-//            return self.field.pow(p);
-//        }
-//    };
-//}
+
+pub fn Signature() type {
+    return struct {
+        r: u256,
+        s: u256,
+
+        pub fn init(r: u256, s: u256) @This() {
+            return .{ .r = r, .s = s };
+        }
+    };
+}
 
 pub fn S256Point() type {
     return struct {
@@ -50,12 +65,12 @@ pub fn S256Point() type {
             return @This(){ .point = self.point.rmul(coef) };
         }
 
-        pub fn verify(self: @This(), z: u256, sig: Signature) bool {
-            const s_inv = modpow(sig.s, N - 2, N);
-            const u = (z * s_inv) % N;
-            const v = (sig.r * s_inv) % N;
+        pub fn verify(self: @This(), z: u256, sig: Signature()) bool {
+            const s_inv = modpow(u256, sig.s, N - 2, N);
+            const u = modmul(u256, z, s_inv, N);
+            const v = modmul(u256, sig.r, s_inv, N);
             const total = G.mul(u).point.add(self.mul(v).point);
-            return total.x == sig.r;
+            return total.x.?.num == sig.r;
         }
 
         //pub fn sec(self: @This(), compressed: bool) {
@@ -64,17 +79,43 @@ pub fn S256Point() type {
     };
 }
 
-const GX: u256 = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798;
-const GY: u256 = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8;
-const G = S256Point().init(GX, GY);
+const G = S256Point().init(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8);
 
-pub fn Signature() type {
-    return struct {
-        r: u256,
-        s: u256,
+test "order" {
+    //@import("std").testing.refAllDeclsRecursive(@This());
+    const s256point = G.mul(N);
+    try std.testing.expectEqual(null, s256point.point.x);
+}
 
-        pub fn init(r: u256, s: u256) @This() {
-            return .{ .r = r, .s = s };
-        }
+test "pubpoint" {
+    // secret, x, y
+    const points: [4][3]u256 = .{
+        .{ 7, 0x5cbdf0646e5db4eaa398f365f2ea7a0e3d419b7e0330e39ce92bddedcac4f9bc, 0x6aebca40ba255960a3178d6d861a54dba813d0b813fde7b5a5082628087264da },
+        .{ 1485, 0xc982196a7466fbbbb0e27a940b6af926c1a74d5ad07128c82824a11b5398afda, 0x7a91f9eae64438afb9ce6448a1c133db2d8fb9254e4546b6f001637d50901f55 },
+        .{ 0x100000000000000000000000000000000, 0x8f68b9d2f63b5f339239c1ad981f162ee88c5678723ea3351b7b444c9ec4c0da, 0x662a9f2dba063986de1d90c2b6be215dbbea2cfe95510bfdf23cbf79501fff82 },
+        .{ 0x1000000000000000000000000000000000000000000000000000080000000, 0x9577ff57c8234558f293df502ca4f09cbc65a6572c842b39b366f21717945116, 0x10b49c67fa9365ad7b90dab070be339a1daf9052373ec30ffae4f72d5e66d053 },
     };
+
+    for (points) |point| {
+        const p = S256Point().init(point[1], point[2]);
+        try testing.expect(G.mul(point[0]).point.eq(p.point));
+    }
+}
+
+test "verify" {
+    const point = S256Point().init(0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c, 0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34);
+    {
+        const z: u256 = 0xec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60;
+        const r: u256 = 0xac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395;
+        const s: u256 = 0x68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4;
+        const sig = Signature().init(r, s);
+        try testing.expect(point.verify(z, sig));
+    }
+    {
+        const z: u256 = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d;
+        const r: u256 = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c;
+        const s: u256 = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6;
+        const sig = Signature().init(r, s);
+        try testing.expect(point.verify(z, sig));
+    }
 }
